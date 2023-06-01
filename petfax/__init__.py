b6a3166c0b38741e122e830bf7867ea739593d00
INS140 <IsaacNSaettel@gmail.com>
2023-06-01 00:32:06 -0700

    small app

---
+++ petfax/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from . import pet
+
+def create_app(): 
+  app = Flask(__name__)
+  
+  app.register_blueprint(pet.bp)
+
+  @app.route('/')
+  def hello(): 
+    return 'Hello, PetFax!'
+
+  return app
